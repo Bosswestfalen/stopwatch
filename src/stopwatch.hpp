732d40c3ee582e21ab402a17ad26b4547bb0a913
Bosswestfalen <24429152+Bosswestfalen@users.noreply.github.com>
2019-02-15 22:33:41 +0100

    :sparkles: implement first version
    Take current time in ctor.
    Calculate time spent in dtor and "send" to a target.
    
    Type checks added to target.

--- src/stopwatch.hpp
+++ src/stopwatch.hpp
@@ -1,7 +1,48 @@
 #ifndef BOSSWESTFALEN_STOPWATCH_HPP_
 #define BOSSWESTFALEN_STOPWATCH_HPP_
 
-// to come
+#include <chrono>
+#include <type_traits>
+
+
+namespace bosswestfalen
+{
+
+using time_type = decltype (std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+template <typename T>
+class stopwatch final
+{
+    static_assert(std::is_nothrow_assignable_v<T&, time_type>
+                  or std::is_nothrow_invocable_v<T&, time_type>);
+
+  public:
+    stopwatch(T& target) noexcept
+    : m_target{target},
+      m_start{std::chrono::high_resolution_clock::now()}
+    {
+    }
+
+    ~stopwatch() noexcept
+    {
+        auto const end = std::chrono::high_resolution_clock::now();
+        auto const duration = std::chrono::duration_cast<std::chrono::nanoseconds> (end - m_start).count();
+
+        if constexpr (std::is_nothrow_assignable_v<T&, time_type>)
+        {
+            m_target = duration;
+        }
+        else // std::is_nothrow_invocable_v == true
+        {
+            m_target(duration);
+        }
+    }
+
+  private:
+    T& m_target;
+    decltype(std::chrono::high_resolution_clock::now()) const m_start;
+};
+}
 
 #endif
 
